ccbdd0339cfb4ecc2712369ed625dae5aa1000bb
LeonLuna98 <conradolunap@gmail.com>
2019-05-09 19:48:05 -0300

    todo comit

---
+++ TP6-EJERCICIO2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6_EJERCICIO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                Console.Write("\n1. SUMAR\n");
+                Console.Write("2. RESTART\n");
+                Console.Write("3. MULTIPLICAR\n");
+                Console.Write("4. DIVIDIR\n");
+                Console.Write("0. SALIR\n");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                    {
+                            int numeros, aux = 0, num;
+                            int i;
+                            Console.Write("Para dejar de sumar Ingrese 00\n");
+                            Console.Write("\n\n");
+                            num = int.Parse(Console.ReadLine());
+                            aux = num;
+                            while(num != 00)
+                            { 
+                                Console.Write("+ \n");
+                                num = int.Parse(Console.ReadLine());
+                                aux += num;
+                            }
+                            Console.Write("=");
+                            Console.Write(aux);
+                    }
+                    break;
+                    case 2:
+                    {
+                        int numeros, aux = 0, num;
+                        int i;
+                        Console.Write("Para dejar de Restar Ingrese 00\n");
+                        Console.Write("\n\n");
+                        num = int.Parse(Console.ReadLine());
+                        aux = num;
+                        while (num != 00)
+                        {
+                            Console.Write(" - \n");
+                            num = int.Parse(Console.ReadLine());
+                            aux -= num;
+                        }
+                        Console.Write("=");
+                        Console.Write(aux);
+                    }
+                    break;
+                    case 3:
+                    {
+                        int numeros, aux = 0, num;
+                        int i;
+                        Console.Write("Para dejar de Multiplicar Ingrese 1\n");
+                        Console.Write("\n\n");
+                        num = int.Parse(Console.ReadLine());
+                        aux = num;
+                        while (num != 1)
+                        {
+                            Console.Write(" * \n");
+                            num = int.Parse(Console.ReadLine());
+                            aux *= num;
+                        }
+                        Console.Write("=");
+                        Console.Write(aux);
+                    }
+                    break;
+                    case 4:
+                    {
+                        int A,B, aux = 0;
+                        Console.Write("Ingrese Numero A\n");
+                        A = int.Parse(Console.ReadLine());
+                        Console.Write("Numero B, B != 0\n");
+                        B = int.Parse(Console.ReadLine());
+                        if (B == 0)
+                        {
+                            Console.Write("No se puede dividir en 0\n");
+                            break;
+                        }
+                        else
+                        {
+                                aux = A / B;
+                                Console.WriteLine("La division es : ");
+                                Console.Write(aux);
+                            }
+                    }
+                    break;
+                }
+            }
+            while (opcion != 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP6GETTARAGUSTIN.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miconsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese Numero a invertir");
+            int a;
+            int aux;
+            int i;
+            a = int.Parse(Console.ReadLine());
+            if (a <= 0)
+            {
+                Console.Write("Este numero no se puede invertir\n");
+                Console.ReadKey();
+            }
+            else
+            {
+                aux = a;
+                int aux2;
+                while (aux != 0)
+                {
+                    aux2 = aux % 10;
+                    Console.Write(aux2);
+                    aux = aux / 10;
+                }
+            }  
+        }
+    }
+}
