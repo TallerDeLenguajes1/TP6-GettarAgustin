020e00283c008bf000579771984f21609933d37d
AgustinGettar <30597247+AgustinGettar@users.noreply.github.com>
2019-05-16 12:46:41 -0300

    Add files via upload
    ejercicio 3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GETTAR_AGUSTIN_EJ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero > 0\n");
+         
+            float num, resultado;
+            num = float.Parse(Console.ReadLine());
+            resultado = Math.Abs(num);
+            Console.Write($"El valor absoluto es: {resultado}\n\n");
+            Console.Write($"El cuadrado es: {Math.Pow(num, 2)}\n\n");
+            Console.Write($"La raiz cuadrada es: {Math.Sqrt(Convert.ToSingle(num))}\n\n");
+            Console.Write($"El seno es: {Math.Sin(Convert.ToSingle(num))}\n\n");
+            Console.Write($"El coseno es: {Math.Cos(Convert.ToSingle(num))}\n\n");
+            Console.Write($"La parte entera de un float: {Math.Round(num)}\n\n");
+            Console.ReadKey();
+        }
+    }
+}
